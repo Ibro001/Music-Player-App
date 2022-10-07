ca8011501e7cada04f3468213c5bb8790a609e3a
Ibro001 <ibrahimmoses56@gmail.com>
2022-10-07 22:57:00 +0100

    imported react

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 
 import { Error, Loader, SongCard } from '../components';
 import { selectGenreListId } from '../redux/features/playerSlice';
